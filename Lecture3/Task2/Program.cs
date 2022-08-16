c07c1161274d82a2987fcf8e1f6dd944885c5615
Ildar-Ysupov <aismen56@mail.ru>
2022-08-16 20:01:11 +0500

    Lecture3

---
+++ Lecture3/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿//  синтаксис цикла
+// for примерно следующий. собирает всё и одну строку
+
+string Method4(int count, string text)
+{
+   string result = String.Empty;
+   for( int i = 0; i < count; i++)
+   {
+       result = result + text;
+   }
+   return result;
+}
+
+string res = Method4(10, "z");
+Console.WriteLine(res);
