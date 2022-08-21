db54fdbd94e4fecf4d92101b1614314edc670abe
Ildar-Ysupov <aismen56@mail.ru>
2022-08-21 20:21:04 +0500

    Lecture4

---
+++ Lecture4/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿//  1 Создание матрицы
+// Начнём с первого примера — напишем таблицу строк. Тип данных string вы уже должны были 
+// запомнить. Дальше — new string. Указываем, что у нас будет 2 строки и 5 столбцов
+
+// string[,] table = new string[2,5];
+// // String.Empty
+// // table [0,0] table [0,1] table [0,2] table [0,4]
+// // table [1,0] table [1,1] table [1,2] table [1,4]
+// table[1, 2] = "слово";
+// for (int rows = 0; rows < 2; rows++)
+// {
+//  for (int columns = 0; columns < 5; columns++)
+//  {
+//  Console.WriteLine($"-{table[rows, columns]}-");
+//  }
+// }
+
+// Заполнение матрицы случайными числами
+
+void PrintArray(int[,] matr)
+{
+ for (int i = 0; i < matr.GetLength(0); i++)
+ {
+ for (int j = 0; j < matr.GetLength(1); j++)
+ {
+ Console.Write($"{matr[i, j]} ");
+ }
+ Console.WriteLine();
+ }
+}
+void FillArray(int[,] matr)
+{
+ for (int i = 0; i < matr.GetLength(0); i++)
+ {
+ for (int j = 0; j < matr.GetLength(1); j++)
+ {
+ matr[i,j] = new Random().Next(1,10);//[1; 10)
+ }
+ }
+}
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
